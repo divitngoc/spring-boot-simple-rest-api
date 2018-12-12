d94e99432a5d35f7d9a424c2557dd4c67f13796b
Divit Bui <divitngoc@gmail.com>
2018-12-12 17:59:35 +0000

    Remove componentScan for arrays

--- com.divit.spring-boot-simple-rest-api/src/main/java/com/divit/springboot/application/Application.java
+++ com.divit.spring-boot-simple-rest-api/src/main/java/com/divit/springboot/application/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.divit.springboot.application"})
+@ComponentScan("com.divit.springboot.application")
 public class Application {
 
 	public static void main(String[] args) {
