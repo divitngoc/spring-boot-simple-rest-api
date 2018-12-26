59c6252c9f17769a6a9bc27c9814f16b0193848b
Divit Bui <divitngoc@gmail.com>
2018-12-26 11:26:06 +0000

    Update dockerfile
    To build docker image:
    docker build -f Dockerfile -t docker-spring-boot-api .
    
    To run container:
    docker run -p 8888:8888 docker-spring-boot-api

--- com.divit.spring-boot-simple-rest-api/src/main/java/com/divit/springboot/application/util/CandidatesUtil.java
+++ com.divit.spring-boot-simple-rest-api/src/main/java/com/divit/springboot/application/util/CandidatesUtil.java
@@ -19,9 +19,10 @@ public final class CandidatesUtil {
 	public static List<Candidate> getCandidates() {
 		var candidateList = List.of(
 				new Candidate("John", 2, Set.of("java", "c#", "c++", "golang")),
-				new Candidate("David", 1, Set.of("javaScript", "c++")),
+				new Candidate("David", 1, Set.of("javascript", "c++")),
 				new Candidate("Diana", 4, Set.of("java", "python")),
-				new Candidate("June", 2, Set.of("ruby", "aws", "docker")));
+				new Candidate("June", 2, Set.of("ruby", "aws", "docker")),
+				new Candidate("April", 3, Set.of("python", "Erlang", "docker")));
 
 		return candidateList;
 	}
